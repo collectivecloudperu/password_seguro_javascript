f47eecc94474fca043fb2f565bb5e3f318f30c0e
collectivecloudperu <collectivecloudperu@gmail.com>
2023-04-19 00:42:05 -0500

    Cambios

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function validarPassword(password) {
+    const decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+    if(password.value.match(decimal)) {
+
+        alert("El password es seguro !"); 
+
+    } else {
+
+        alert("El password debe contener al menos una minúscula, mayúscula, número y un carácter especial. Y 8 carácteres como mínimo.")
+
+    }
+
+
+}
